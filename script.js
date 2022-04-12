118477e45d7ba6e0a6e01d5bad51e902bd2e6f0e
Viktor <victorpatrin@gmail.com>
2022-04-12 19:35:37 +0700

    mobile version

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ window.onload = function() {
             taskText.classList.add('task');
             closeBtn.classList.add('close-btn', 'hide');
             closeBtn.innerHTML = '<path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/>';
+            //closeBtn.setAttribute('viewBox', '0 0 1rem 1rem');
             taskText.textContent = newTask.value;
             todoList.prepend(todoItem);
             todoItem.prepend(checkBtn);
